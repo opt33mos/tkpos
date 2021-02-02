fcdbac41ee40e83775f3ba4e2fe3b3fb4014ac0d
Eurika Mercado <lilsmile214@gmail.com>
2021-02-02 20:55:37 +0800

    Initial Commit

---
+++ resources/js/app.js
@@ -0,0 +1,26 @@
+require('./bootstrap');
+import VueRouter from "vue-router";
+window.Vue = require('vue').default;
+
+Vue.use(VueRouter);
+
+let routes = [
+    {
+        path: "/dashboard",
+        component: require("./components/Dashboard.vue").default,
+    },
+    {
+        path: "/example",
+        component: require("./components/Example.vue").default,
+    },
+];
+
+const router = new VueRouter({
+    mode: 'history',
+    routes, // short for `routes: routes`
+});
+
+const app = new Vue({
+    el: '#app',
+    router
+});
